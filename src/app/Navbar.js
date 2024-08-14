2f10973d85043e2662e71c75da2f5427d8988a37
Garrett Adams <garrettadams27@gmail.com>
2024-08-13 22:25:56 -0600

    mobile home, navbar

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -6,38 +6,56 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
-    const [dropdown, setDropdown] = useState(null);
-    const dropdownRef = useRef(null);
-    const pathname = usePathname();
-  
-    const toggleDropdown = (menu) => {
-      setDropdown(dropdown === menu ? null : menu);
+  const [dropdown, setDropdown] = useState(null);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const dropdownRef = useRef(null);
+  const pathname = usePathname();
+
+  const toggleMobileMenu = () => {
+    setMobileMenu(!mobileMenu)
+  }
+
+  const toggleDropdown = (menu) => {
+    setDropdown(dropdown === menu ? null : menu);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdown(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-  
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdown(null);
-      }
-    };
-  
-    useEffect(() => {
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }, []);
-  
-    const isActive = (path) => pathname === path;
+  }, []);
+
+  const isActive = (path) => pathname === path;
+
+  const handleLinkClick = () => {
+    setDropdown(null); // Close the dropdown
+  };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow-sm">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-[82px]">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-[82px] px-3 relative">
         <Link href="/">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
           </div>
         </Link>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+
+        <button onClick={toggleMobileMenu} type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+
+
+        {/* <div className={`${mobileMenu ? 'block' : 'hidden'} w-full md:block md:w-auto z-50`}>
           <ul className="flex flex-col font-sm p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-11 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 mx-auto md:justify-center">
             <li>
               <Link href="/">
@@ -155,8 +173,130 @@ export default function Navbar() {
               </svg>
             </li>
           </ul>
-        </div>
-        <div className='flex items-bottom space-x-1.5'>
+        </div> */}
+
+        <div className={`${mobileMenu ? 'block' : 'hidden'} w-full md:block md:w-auto z-50`}>
+        <ul className="flex flex-col font-sm p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-11 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 mx-auto md:justify-center">
+          <li>
+            <Link href="/">
+              <div className={`${isActive('/') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>home</div>
+            </Link>
+          </li>
+          <li className="relative" ref={dropdownRef}>
+            <button onClick={() => toggleDropdown('firstTime')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">first time here
+              <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+            {dropdown === 'firstTime' && (
+              <div className="absolute left-0 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-base text-gray-700 dark:text-gray-400">
+                  <li>
+                    <Link href="/first-time-here" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">first time here?</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/leadership" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">leadership</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/beliefs" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">beliefs</div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li className="relative" ref={dropdownRef}>
+            <button onClick={() => toggleDropdown('yourFamily')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">your family
+              <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+            {dropdown === 'yourFamily' && (
+              <div className="absolute left-0 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-base text-gray-700 dark:text-gray-400">
+                  <li>
+                    <Link href="/refresh-kids" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">refresh kids</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/refresh-youth" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">refresh youth</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/mens-ministry" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">men&apos;s ministry</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/womens-ministry" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">women&apos;s ministry</div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li className="relative" ref={dropdownRef}>
+            <button onClick={() => toggleDropdown('connect')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">connect
+              <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+            {dropdown === 'connect' && (
+              <div className="absolute left-0 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-base text-gray-700 dark:text-gray-400">
+                  <li>
+                    <Link href="/discovery" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">discovery</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/small-groups" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">small groups</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/serve-team" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">serve team</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/sermons" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">sermons</div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/events" onClick={handleLinkClick}>
+                      <div className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">events</div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li>
+            <Link href="/contact">
+              <div className={`${isActive('/contact') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>contact</div>
+            </Link>
+          </li>
+          <li className='flex items-center'>
+            <div href="https://www.merriam-webster.com/dictionary/give" className="text-xl block py-2 px-3 me-1 text-primary rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">give</div>
+            <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
+            </svg>
+          </li>
+        </ul>
+      </div>
+
+
+        <div className='flex items-bottom space-x-1.5 md:flex hidden'>
           <a href="/ig">
             <svg className=" w-8 h-8 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path fill="currentColor" fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd" />
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import discoveryMobile from '../../public/discovery-mobile.webp'
@@ -9,146 +12,235 @@ import kidsDesktop from '../../public/r-kids-desktop.webp'
 import leadershipImg from '../../public/tj.webp'
 
 export default function Home() {
-  const boxes = [
-    {
-      width: 'w-[234px]',
-      title: 'First Time Here!',
-      label: 'First Time Here',
-      imageSrc: '',
-      imageSrcMobile: '',
-      link: '/first-time-here',
-      button: true,
-      button_color: 'primary',
-      button_label: 'Learn More',
-    },
-    {
-      width: 'w-[462px]',
-      title: 'Intro video/slide',
-      label: null,
-      imageSrc: '',
-      imageSrcMobile: '',
-      link: null,
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-    {
-      width: 'w-[234px]',
-      title: 'Leadership',
-      label: 'Leadership',
-      imageSrc: leadershipImg,
-      imageSrcMobile: ' ',
-      link: '/leadership',
-      button: true,
-      button_color: 'white',
-      button_label: 'Learn More',
-    },
-    {
-      width: 'w-[310px]',
-      title: 'Kids',
-      label: null,
-      imageSrc: kidsDesktop,
-      imageSrcMobile: kidsMobile,
-      link: '/r-kids',
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-    {
-      width: 'w-[310px]',
-      title: 'Connected',
-      label: null,
-      imageSrc: connectedDesktop,
-      imageSrcMobile: connectedMobile,
-      link: '/connect',
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-    {
-      width: 'w-[310px]',
-      title: 'Discovery',
-      label: null,
-      imageSrc: discoveryDesktop,
-      imageSrcMobile: discoveryMobile,
-      link: '/discovery',
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-    {
-      width: 'w-[462px]',
-      title: 'Events',
-      label: null,
-      imageSrc: '',
-      imageSrcMobile: ' ',
-      link: '/events',
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-    {
-      width: 'w-[462px]',
-      title: 'Sermon',
-      label: null,
-      imageSrc: '',
-      imageSrcMobile: ' ',
-      link: '/sermons',
-      button: false,
-      button_color: null,
-      button_label: null,
-    },
-  ];
+  const [showMobile, setShowMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1025) {
+        setShowMobile(true);
+      } else {
+        setShowMobile(false);
+      }
+    };
+
+    handleResize(); // Check on initial load
+    window.addEventListener('resize', handleResize); // Listen for resize events
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // Cleanup on unmount
+    };
+  }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-2 sm:px-4 py-8">
       <div className="flex flex-wrap justify-center gap-4">
-        <div className="flex flex-wrap gap-4 justify-center mb-4">
-          {boxes.slice(0, 3).map((box, index) => (
-            <Box key={index} {...box} />
-          ))}
-        </div>
-        <div className="flex flex-wrap gap-4 justify-center mb-4">
-          {boxes.slice(3, 6).map((box, index) => (
-            <Box key={index} {...box} />
-          ))}
-        </div>
-        <div className="flex flex-wrap gap-4 justify-center">
-          {boxes.slice(6).map((box, index) => (
-            <Box key={index} {...box} />
-          ))}
-        </div>
+        {showMobile ? (
+          <>
+            <div className="flex flex-wrap gap-4 justify-center mb-4">
+              <Link href="/first-time-here" className="bg-white shadow-md rounded-2xl w-[234px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                <h3 className="text-2xl font-semibold absolute top-1.5 left-3 text-white">First Time Here!</h3>
+                <div className="absolute right-0 bottom-0">
+                  <button type="button" className='focus:outline-none focus:ring-3 focus:ring-primaryLight font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-primary text-white hover:bg-primaryDark'>
+                    Learn More
+                  </button>
+                </div>
+              </Link>
+
+              <div className="bg-white shadow-md rounded-2xl w-[462px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                {/* No button or title for this box */}
+              </div>
+
+              <Link href="/leadership" className="bg-white shadow-md rounded-2xl w-[234px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={leadershipImg}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Leadership Image"
+                  />
+                </div>
+                <h3 className="text-2xl font-semibold absolute top-1.5 left-3 text-white">Leadership</h3>
+                <div className="absolute right-0 bottom-0">
+                  <button type="button" className='focus:outline-none focus:ring-3 focus:ring-white-200 font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-white text-black hover:bg-gray-200'>
+                    Learn More
+                  </button>
+                </div>
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4 justify-center mb-4">
+              <Link href="/r-kids" className="bg-white shadow-md rounded-2xl w-[310px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={kidsDesktop}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Kids Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+
+              <Link href="/connect" className="bg-white shadow-md rounded-2xl w-[310px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={connectedDesktop}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Connected Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+
+              <Link href="/discovery" className="bg-white shadow-md rounded-2xl w-[310px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={discoveryDesktop}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Discovery Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4 justify-center">
+              <Link href="/events" className="bg-white shadow-md rounded-2xl w-[462px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+
+              <Link href="/sermons" className="bg-white shadow-md rounded-2xl w-[462px] h-[250px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+            </div>
+          </>
+        ) : (
+          <>
+            {/* row 1 */}
+            <div className="flex flex-wrap gap-4 justify-center mb-4">
+              <div className="bg-white shadow-md rounded-2xl w-[366px] h-[198px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                {/* No button or title for this box */}
+              </div>
+            </div>
+
+            {/* row 2 */}
+            <div className="flex flex-wrap gap-4 justify-center mb-4">
+              <Link href="/first-time-here" className="bg-white shadow-md rounded-2xl w-[170px] h-[180px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  {/* Add your image for this box here */}
+                </div>
+                <h3 className="text-2xl font-semibold absolute top-1.5 left-3 text-white">First Time Here!</h3>
+                <div className="absolute right-0 bottom-0">
+                  <button type="button" className='focus:outline-none focus:ring-3 focus:ring-primaryLight font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-primary text-white hover:bg-primaryDark'>
+                    Learn More
+                  </button>
+                </div>
+              </Link>
+              <Link href="/leadership" className="bg-white shadow-md rounded-2xl w-[170px] h-[180px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={leadershipImg}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Leadership Image"
+                  />
+                </div>
+                <h3 className="text-2xl font-semibold absolute top-1.5 left-3 text-white">Leadership</h3>
+                <div className="absolute right-0 bottom-0">
+                  <button type="button" className='focus:outline-none focus:ring-3 focus:ring-white-200 font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-white text-black hover:bg-gray-200'>
+                    Learn More
+                  </button>
+                </div>
+              </Link>
+            </div>
+
+            {/* row 3 */}
+            <div className="flex flex-wrap gap-4 justify-center mb-4">
+              <Link href="/r-kids" className="bg-white shadow-md rounded-2xl w-[366px] h-[52px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={kidsMobile}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Kids Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+
+              <Link href="/connect" className="bg-white shadow-md rounded-2xl w-[366px] h-[52px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={connectedMobile}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Connected Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+
+              <Link href="/discovery" className="bg-white shadow-md rounded-2xl w-[366px] h-[52px] relative overflow-hidden">
+                <div className="relative block h-full">
+                  <Image
+                    src={discoveryMobile}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Discovery Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+            </div>
+            {/* row 4 */}
+            <div className="flex flex-wrap gap-4 justify-center">
+              <Link href="/events" className="bg-white shadow-md rounded-2xl w-[368px] h-[199px] relative overflow-hidden">
+                <div className="relative block h-full">
+                <Image
+                    src={kidsMobile}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="event Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+            </div>
+            {/* row 5 */}
+            <div className="flex flex-wrap gap-4 justify-center">
+              <Link href="/sermons" className="bg-white shadow-md rounded-2xl w-[368px] h-[199px] relative overflow-hidden">
+                <div className="relative block h-full">
+                <Image
+                    src={kidsMobile}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="sermon Image"
+                  />
+                </div>
+                {/* No button or title for this box */}
+              </Link>
+            </div>
+
+
+
+          </>
+        )}
       </div>
     </div>
   );
 }
-
-const Box = ({ width, title, label, imageSrc, imageSrcMobile, link, button, button_color, button_label }) => {
-
-  return (
-    <Link href='' className={`bg-white shadow-md rounded-2xl ${width} h-[250px] relative overflow-hidden`}>
-      <div to={link} className="relative block h-full">
-        <Image 
-          src={imageSrc} 
-          layout="fill" 
-          objectFit="cover" 
-          alt="alt" 
-        />
-      </div>
-      {label && (
-        <h3 className="text-2xl font-semibold absolute top-1.5 left-3 text-white">{title}</h3>
-      )}
-      <div className="absolute right-0 bottom-0">
-        {button && (
-          <>
-            {button_color === 'primary' ? (
-              <button type="button" className='focus:outline-none focus:ring-3 focus:ring-primaryLight font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-primary text-white hover:bg-primaryDark'>{button_label}</button>
-            ) : (
-              <button type="button" className='focus:outline-none focus:ring-3 focus:ring-white-200 font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 bg-white text-black hover:bg-gray-200'>{button_label}</button>
-            )}
-          </>
-        )}
-      </div>
-    </Link>
-  );
-};
