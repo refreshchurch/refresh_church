00babbb89bca21fb6afdd5580f682a8dafcfa3a8
Garrett Adams <garrettadams27@gmail.com>
2024-11-07 15:09:34 -0700

    Cleaned up img's and added proper links to home page

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -1,5 +1,5 @@
 "use client"
-import { AlarmClock, ArrowUpRight, Book, Cable, Calendar, Map, Plug, Shapes, Tv, User, User2, Users } from "lucide-react";
+import { AlarmClock, ArrowUpRight, Book, Boxes, Cable, Calendar, HandCoins, Handshake, Map, Plug, Shapes, Tv, User, User2, Users, Waves } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -55,21 +55,28 @@ export default function HomePage({ showMobile, hydrated }) {
         </div>
 
         <div className="flex flex-col gap-4">
-          <div className="bg-white shadow-md rounded-2xl w-full h-[40%] relative overflow-hidden p-6">
+          <div className="bg-white shadow-md rounded-2xl w-full h-[35%] relative overflow-hidden p-6">
             <h1 className="text-black text-3xl font-bold pb-[1px]">Learn More</h1>
-            <h3 className="text-gray-400 text-lg">Short little description</h3>
+            <h3 className="text-gray-400 text-lg">Stuff for you</h3>
             <ul className="mt-2 gap-y-2">
-              <Link href="/leadership" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
-                <User className="w-7" />
+              <Link href="/r-kids" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+                <Shapes className="w-7" />
                 <p className="text-xl font-semibold">
-                  Leadership
+                  Kids
                 </p>
               </Link>
-              <Link href="/beliefs" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+              <Link href="/refresh-youth" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
                 {/* <Plug/> */}
-                <Book className="w-7" />
+                <Waves className="w-7" />
                 <p className="text-xl font-semibold">
-                  Beliefs
+                  Youth
+                </p>
+              </Link>
+              <Link href="/small-groups" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+                {/* <User className="w-7" /> */}
+                <Boxes className="w-7" />
+                <p className="text-xl font-semibold">
+                  Groups
                 </p>
               </Link>
               <Link href="/sermons" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
@@ -78,17 +85,11 @@ export default function HomePage({ showMobile, hydrated }) {
                   Sermons
                 </p>
               </Link>
-              <Link href="/" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
-                <Shapes className="w-7" />
-                <p className="text-xl font-semibold">
-                  Kids
-                </p>
-              </Link>
             </ul>
           </div>
-          <div className="bg-white shadow-md rounded-2xl w-full h-[60%] relative overflow-hidden p-6">
+          <div className="bg-white shadow-md rounded-2xl w-full h-[65%] relative overflow-hidden p-6">
             <h2 className="text-black text-3xl font-bold">Get Involved</h2>
-            <h3 className="text-gray-400 text-xl">Short little description</h3>
+            <h3 className="text-gray-400 text-xl ps-0.5">Next Steps</h3>
             <ul className="mt-2 gap-y-2">
               <Link href="/connect" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
                 <Cable className="w-7" />
@@ -96,10 +97,16 @@ export default function HomePage({ showMobile, hydrated }) {
                   Get Connected
                 </p>
               </Link>
-              <Link href="/" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+              <Link href="https://refresh.churchcenter.com/people/forms/301611?_ga=2.212286996.1630180229.1675698110-905807684.1675698110" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer" target="_blank" rel="noopener noreferrer">
                 <Users className="w-7" />
                 <p className="text-xl font-semibold">
                   Serve
+                </p>
+              </Link>
+              <Link href="https://refresh.churchcenter.com/giving?open-in-church-center-modal=true" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer" target="_blank" rel="noopener noreferrer">
+                <Handshake className="w-7" />
+                <p className="text-xl font-semibold">
+                  Give
                 </p>
               </Link>
               <Link href="/" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
@@ -120,7 +127,7 @@ export default function HomePage({ showMobile, hydrated }) {
                   Owhyee High School
                 </p>
               </Link> */}
-              <Link href="/" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+              {/* <Link href="/" className="flex items-center gap-2 px-2 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
                 <div className="overflow-hidden rounded-full w-12 h-12">
                   <Image
                     src="/photos/placeholder.webp"
@@ -132,9 +139,41 @@ export default function HomePage({ showMobile, hydrated }) {
                   />
                 </div>
                 <p className="text-xl font-semibold">
-                  Event 1
+                  Event Design 1
                 </p>
-                <ArrowUpRight className="w-7"/>
+                <ArrowUpRight className="w-7" />
+              </Link>
+              <Link href="/" className="flex items-center gap-5 px-3 py-3 hover:bg-gray-100 rounded-lg cursor-pointer border border-gray-300 shadow-sm">
+                <div className="overflow-hidden rounded-full w-12 h-12">
+                  <Image
+                    src="/photos/placeholder.webp"
+                    width={48}
+                    height={48}
+                    alt="Leadership Image"
+                    draggable="false"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <p className="text-xl font-semibold">
+                  Event Design 2
+                </p>
+                <ArrowUpRight className="w-7" />
+              </Link> */}
+              <Link href="/" className="flex items-center gap-5 px-3 py-3 hover:bg-gray-100 rounded-lg cursor-pointer">
+                <div className="overflow-hidden rounded-full w-12 h-12">
+                  <Image
+                    src="/photos/placeholder.webp"
+                    width={48}
+                    height={48}
+                    alt="Leadership Image"
+                    draggable="false"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <p className="text-xl font-semibold">
+                  Event Design 3
+                </p>
+                <ArrowUpRight className="w-7" />
               </Link>
             </ul>
           </div>
@@ -147,26 +186,26 @@ export default function HomePage({ showMobile, hydrated }) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
         <Link href="/first-time-here" className="bg-white shadow-md rounded-2xl w-full aspect-video relative overflow-hidden transform transition-transform duration-350 hover:scale-[1.02]">
           <Image
-            src="/photos/placeholder.webp"
+            src="/photos/blue16_9.webp"
             layout="fill"
             objectFit="cover"
             alt="Leadership Image"
             draggable="false"
           />
           <div className="absolute inset-0 bg-black bg-opacity-10 flex items-center justify-center">
-            <p className="text-white sm:text-7xl text-5xl font-bold z-100">Placeholder</p>
+            <p className="text-white sm:text-7xl text-5xl font-bold z-100">Community</p>
           </div>
         </Link>
-        <Link href="/first-time-here" className="bg-white shadow-md rounded-2xl w-full aspect-video relative overflow-hidden transform transition-transform duration-350 hover:scale-[1.02]">
+        <Link href="/discovery" className="bg-white shadow-md rounded-2xl w-full aspect-video relative overflow-hidden transform transition-transform duration-350 hover:scale-[1.02]">
           <Image
-            src="/photos/placeholder.webp"
+            src="/photos/orange16_9.webp"
             layout="fill"
             objectFit="cover"
             alt="Leadership Image"
             draggable="false"
           />
           <div className="absolute inset-0 bg-black bg-opacity-10 flex items-center justify-center">
-            <p className="text-white sm:text-7xl text-5xl font-bold z-100">Placeholder</p>
+            <p className="text-white sm:text-7xl text-5xl font-bold z-100">Discovery</p>
           </div>
         </Link>
 
