ca37e0f254669b82c9b271900c07c27d0ebe437a
Garrett Adams <garrettadams27@gmail.com>
2024-11-22 18:23:08 -0700

    connect w/ us white bg

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -214,30 +214,30 @@ export default function HomePage() {
 
       <div className="flex flex-wrap gap-4 justify-center mb-4">
         <div className="bg-white shadow-md rounded-2xl w-full h-[200px] sm:h-[300px] lg:h-[350px] relative overflow-hidden">
-          <Image
+          {/* <Image
             src='/photos/background/WhiteBlue16_9.webp'
             layout="fill"
             objectFit="cover"
             alt="white and orange background Image"
             draggable="false"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center flex-col">
-            <h2 className="text-white text-2xl sm:text-5xl font-bold z-100 mb-1 mt-5"> Connect with us Online</h2>
+          /> */}
+          <div className="absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center flex-col">
+            <h2 className="text-gray-900 text-2xl sm:text-5xl font-bold z-100 mb-1 mt-5"> Connect with us Online</h2>
             <div className="flex justify-end space-x-8 mt-3">
               <Link href='https://www.instagram.com/refreshchurchtv/' target="_blank" rel="noopener noreferrer">
-                <svg className=" w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <svg className=" w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path fill="currentColor" fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd" />
                 </svg>
               </Link>
 
               <Link href="https://www.facebook.com/myrefreshchurch" target="_blank" rel="noopener noreferrer">
-                <svg className="pb-0.5 w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <svg className="pb-0.5 w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                   <path fillRule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clipRule="evenodd" />
                 </svg>
               </Link>
 
               <Link href="https://www.youtube.com/@refreshchurchtv" target="_blank" rel="noopener noreferrer">
-                <svg className="w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer mt-1 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <svg className="w-16 h-16 sm:w-20 sm:h-20 hover:scale-110 duration-300 ease-in-out cursor-pointer mt-1 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                   <path fillRule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clipRule="evenodd" />
                 </svg>
               </Link>
