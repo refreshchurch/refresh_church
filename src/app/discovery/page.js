e74fc0e19d9da2ec9153c9df124de40e1d6b0b58
Garrett Adams <garrettadams27@gmail.com>
2024-11-27 14:38:04 -0700

    Discovery page screen size fixes

--- src/app/discovery/page.js
+++ src/app/discovery/page.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Discovery() {
   return (
-    <div className="container mx-auto px-8 sm:px-36 py-12">
+    <div className="container mx-auto px-8  py-12">
       <h2 className="text-4xl font-bold text-gray-900 tracking-tighter sm:text-4xl md:text-5xl mb-12">Discovery Class</h2>
       
       {/* Introduction Section */}
@@ -24,28 +24,28 @@ export default function Discovery() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mb-12">
         {/* Step 1 */}
         <div className="relative bg-[#3fbeb2] shadow-md rounded-2xl flex flex-col justify-center items-center text-center h-80 transform transition-transform duration-300 hover:scale-105">
-          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-center items-center">
-            <h4 className="text-xl font-bold text-white mb-2 uppercase">Step 1: Know God</h4>
+          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-start items-center">
+            <h4 className="text-xl font-bold text-white mb-5 uppercase mt-4">Step 1:</h4>
             <p className="text-white px-4">
-            Discovery Party is a “house party” at Refresh HQ, and an opportunity to connect with our church staff. You’ll also hear more about the history, mission and vision of Refresh. 
+            Discovery Party is an opportunity to connect with our church staff and hear more about the history, mission and vision of Refresh. 
             </p>
           </div>
         </div>
 
         {/* Step 2 */}
-        <div  className="relative bg-[#f39757] shadow-md rounded-2xl flex flex-col justify-center items-center text-center h-80 transform transition-transform duration-300 hover:scale-105">
-          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-center items-center">
-            <h4 className="text-xl font-bold text-white mb-2 uppercase">Step 2: Find Freedom</h4>
+        <div className="relative bg-[#f39757] shadow-md rounded-2xl flex flex-col justify-center items-center text-center md:h-80 h-[350px] transform transition-transform duration-300 hover:scale-105">
+          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-start items-center">
+            <h4 className="text-xl font-bold text-white mb-5 uppercase mt-4">Step 2:</h4>
             <p className="text-white px-4">
-            Discovery Learning is an online assessment of your personality, spiritual gifts, and heart for ministry. It includes an online lesson series to help you understand how God wired you and how you can use that information to discover His purpose in your life. Watch the lessons below.
+            Discovery Learning is an online assessment of your personality, spiritual gifts, and heart for ministry. It includes an online lesson series to help you understand how God wired you and how you can use that information to discover His purpose in your life.
             </p>
           </div>
         </div>
 
         {/* Step 3 */}
-        <div className="relative bg-[#e46246] shadow-md rounded-2xl flex flex-col justify-center items-center text-center h-80 transform transition-transform duration-300 hover:scale-105">
-          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-center items-center">
-            <h4 className="text-xl font-bold text-white mb-2 uppercase">Step 3: Discover Purpose</h4>
+        <div className="relative bg-[#e46246] shadow-md rounded-2xl flex flex-col justify-center items-center text-center lg:h-80 md:h-96 h-[410px] transform transition-transform duration-300 hover:scale-105">
+          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-start items-center">
+            <h4 className="text-xl font-bold text-white mb-5 uppercase mt-4">Step 3:</h4>
             <p className="text-white px-4">
             Discovery Lab is an in-person workshop at Refresh HQ. Our team will get to break down everything you learned throughout the week, process it with you, and help you apply it in your life. At Discovery Lab, you’ll get to discuss with us how your personality, gifts, and experience can guide you into God’s purpose for your life. Lunch and childcare are provided. 
             </p>
@@ -53,9 +53,9 @@ export default function Discovery() {
         </div>
 
         {/* Step 4 */}
-        <div  className="relative bg-[#315860] shadow-md rounded-2xl flex flex-col justify-center items-center text-center h-80 transform transition-transform duration-300 hover:scale-105">
-          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-center items-center">
-            <h4 className="text-xl font-bold text-white mb-2 uppercase">Step 4: Make a Difference</h4>
+        <div className="relative bg-[#315860] shadow-md rounded-2xl flex flex-col justify-center items-center text-center lg:h-80 md:h-96 h-[350px] transform transition-transform duration-300 hover:scale-105">
+          <div className="absolute inset-0 bg-black bg-opacity-20 rounded-2xl p-4 flex flex-col justify-start items-center">
+            <h4 className="text-xl font-bold text-white mb-5 uppercase mt-4">Step 4:</h4>
             <p className="text-white px-4">
             Discovery Launch is an opportunity to launch into fulfilling your purpose here at Refresh. You will get to connect with a Serve Team that aligns with your personality, gifts and passion. Our prayer is that this will be a launch pad for your journey towards fulfilling your purpose! 
             </p>
