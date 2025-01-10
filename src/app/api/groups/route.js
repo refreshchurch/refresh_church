c89be126120692737b81ffe46e8f270f91768627
Garrett Adams <garrettadams27@gmail.com>
2025-01-09 17:40:19 -0700

    groups cacheing issue

--- src/app/api/groups/route.js
+++ src/app/api/groups/route.js
@@ -5,9 +5,9 @@ export async function GET() {
   try {
     const response = await axios.get('https://api.planningcenteronline.com/groups/v2/groups', {
       params: {
-        include: 'enrollment', 
-        include: 'group_type', 
-        per_page: 200,         
+        include: 'enrollment',
+        include: 'group_type',
+        per_page: 200,
       },
       auth: {
         username: process.env.PLANNING_CENTER_APP_ID,
@@ -15,7 +15,10 @@ export async function GET() {
       },
     });
 
-    return NextResponse.json(response.data.data);
+    // Ensure no caching
+    const res = NextResponse.json(response.data.data, { revalidate: 0 }); // Disable Vercel's ISR
+    res.headers.set('Cache-Control', 'no-store'); // Prevent browser caching
+    return res;
   } catch (error) {
     console.error('Error fetching events from Planning Center:', error);
     return NextResponse.json({ error: 'Error fetching events' }, { status: 500 });
