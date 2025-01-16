74005441006920e32fa5e3ccfe4c585edf119bf9
Garrett Adams <garrettadams27@gmail.com>
2025-01-15 20:19:22 -0700

    this is a test push

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -55,6 +55,7 @@ export default function HomePage() {
             Your browser does not support the video tag.
           </video>
           <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center flex-col">
+            THIS IS A TEST SITE
             <Image
               src="/photos/white-logo.webp"
               width={600}
