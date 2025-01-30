26090ae3dd1827c0cbf8e3b5c35492c1800dd8ed
Garrett Adams <garrettadams27@gmail.com>
2025-01-30 15:51:48 -0700

    updates and ios auto play

--- src/app/stories/page.js
+++ src/app/stories/page.js
@@ -1,12 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 // Replace these with your YouTube API key and channel ID
 const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID;
 
-export default function Sermons() {
+export default function Stories() {
   const [mainVideo, setMainVideo] = useState(null);
   const [recentVideos, setRecentVideos] = useState([]);
   const [playlists, setPlaylists] = useState([]);
@@ -74,16 +75,16 @@ export default function Sermons() {
     fetchYouTubeData();
   }, []);
 
-  const handleViewAllClick = () => {
-    window.open("https://www.youtube.com/@refreshchurchtv", "_blank");
-  };
+  // const handleViewAllClick = () => {
+  //   window.open("https://www.youtube.com/@refreshchurchtv", "_blank");
+  // };
 
   if (darkTheme) {
     return (
       <section className="w-full py-8 md:py-10 lg:py-12 bg-zinc-900 text-gray-100">
         <div className="container mx-auto px-8 md:px-24 lg:px-32 space-y-12">
           {/* Large video section */}
-          <h2 className="text-3xl font-bold text-gray-100 tracking-tighter md:text-4xl">Sermons</h2>
+          <h2 className="text-3xl font-bold text-gray-100 tracking-tighter md:text-4xl">Stories</h2>
           <div className="flex justify-center w-full">
             <div className="relative w-full aspect-video overflow-hidden rounded-xl shadow-lg">
               {loading ? (
@@ -98,6 +99,7 @@ export default function Sermons() {
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen
+                      playsinline
                     ></iframe>
                   ) : (
                     <div
@@ -182,7 +184,7 @@ export default function Sermons() {
     <section className="w-full py-8 md:py-10 lg:py-12">
       <div className="container mx-auto px-8 md:px-24 lg:px-32 space-y-12">
         {/* Large video section */}
-        <h2 className="text-3xl font-bold text-gray-900 tracking-tighter md:text-4xl">Sermons</h2>
+        <h2 className="text-3xl font-bold text-gray-900 tracking-tighter md:text-4xl">Stories</h2>
         <div className="flex justify-center w-full">
           <div className="relative w-full aspect-video overflow-hidden rounded-xl shadow-lg">
             {loading ? (
@@ -247,42 +249,16 @@ export default function Sermons() {
           </div>
         </div>
 
-        {/* Sermon Series */}
-        {/* <div className="space-y-6 mt-6 w-full">
-          <h2 className="text-2xl inline-block font-semibold rounded-lg px-3 py-1 text-gray-900">Sermon Series</h2>          
-          <div className="overflow-x-auto flex gap-6 snap-x w-full">
-            {(loading ? staticRecentVideos : playlists).map((playlist, index) => (
-              <div key={index} className="flex-none w-[60%] sm:w-[50%] lg:w-[30%] snap-start">
-                <div className="relative w-full aspect-video overflow-hidden rounded-xl">
-                  {loading ? (
-                    <div className="w-full h-full bg-gray-300 animate-pulse rounded-xl"></div>
-                  ) : (
-                    <iframe
-                      className="w-full h-full object-cover"
-                      src={`https://www.youtube.com/embed/playlist?list=${playlist.id}`}
-                      title={`YouTube playlist player ${index + 1}`}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    ></iframe>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div> */}
-
         <div className="mt-5 w-full flex items-center justify-center">
-          <button onClick={handleViewAllClick} className="flex items-center px-6 py-3 bg-primaryDark text-white font-semibold rounded-lg shadow hover:bg-primary transition duration-300 ease-in-out">
-            View All Stories
+          <Link href={"/sermons"} className="flex items-center px-6 py-3 bg-primaryDark text-white font-semibold rounded-lg shadow hover:bg-primary transition duration-300 ease-in-out">
+            More Videos
             <svg className="w-5 h-5 ms-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
             </svg>
-          </button>
+          </Link>
         </div>
       </div>
       <div className="mt-1 w-full flex items-center justify-center">
-      if we want to keep this button where do we want it to go? ^^^^
       </div>
     </section>
   );
