c43e8ecff31ad5adadeb4a6a35c5b21ad1f620e8
Garrett Adams <garrettadams27@gmail.com>
2025-02-02 11:08:43 -0700

    redirct

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const nextConfig = {
       domains: ['placehold.co', 'example.com', 'another-example.com', 'refresh.church', 'registrations-production.s3.amazonaws.com', 'groups-production.s3.amazonaws.com', 'images.planningcenterusercontent.com'],
       dangerouslyAllowSVG: true,
     },
+    async redirects() {
+      return [
+        {
+          source: '/merch',
+          destination: 'https://your-shopify-store-url.com', // Replace with your actual Shopify store URL
+          permanent: false, // Set to true if this should be a permanent redirect
+        },
+      ];
+    },
   };
   
   export default nextConfig;
