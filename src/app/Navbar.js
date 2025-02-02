4d718616906b82a11ec306b120efcf036f8b9f61
Garrett Adams <garrettadams27@gmail.com>
2025-02-02 11:26:45 -0700

    merch in navbar

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -5,6 +5,7 @@ import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import { ArrowUpRight } from 'lucide-react';
 // import { serverHooks } from 'next/dist/server/app-render/entry-base';
 
 
@@ -75,7 +76,7 @@ export default function Navbar() {
           </div>
         </Link>
 
-        <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg min-[1135px]:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg min-[1210px]:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -203,15 +204,15 @@ export default function Navbar() {
           </ul>
         </div> */}
 
-        <div className={`${mobileMenu ? 'block' : 'hidden'} w-full min-[1135px]:block min-[1135px]:w-auto z-50`}>
-          <ul className="flex flex-col font-sm p-4 min-[1210px]:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 min-[1135px]:space-x-11 rtl:space-x-reverse min-[1135px]:flex-row min-[1135px]:mt-0 min-[1135px]:border-0 min-[1135px]:bg-white dark:bg-gray-800 min-[1135px]:dark:bg-gray-900 dark:border-gray-700 mx-auto min-[1135px]:justify-center">
+        <div className={`${mobileMenu ? 'block' : 'hidden'} w-full min-[1210px]:block min-[1210px]:w-auto z-50`}>
+          <ul className="flex flex-col font-sm p-4 min-[1210px]:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 min-[1210px]:space-x-11 rtl:space-x-reverse min-[1210px]:flex-row min-[1210px]:mt-0 min-[1210px]:border-0 min-[1210px]:bg-white dark:bg-gray-800 min-[1210px]:dark:bg-gray-900 dark:border-gray-700 mx-auto min-[1210px]:justify-center">
             <li>
               <Link href="/">
-                <div className={`${isActive('/') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1135px]:dark:hover:bg-transparent`}>home</div>
+                <div className={`${isActive('/') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1210px]:dark:hover:bg-transparent`}>home</div>
               </Link>
             </li>
             <li className="relative" ref={firstTimeRef}>
-              <button onClick={() => toggleDropdown('firstTime')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 min-[1135px]:w-auto dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1135px]:dark:hover:bg-transparent">first time here
+              <button onClick={() => toggleDropdown('firstTime')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 min-[1210px]:w-auto dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1210px]:dark:hover:bg-transparent">first time here
                 <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
@@ -239,7 +240,7 @@ export default function Navbar() {
               )}
             </li>
             <li className="relative" ref={yourFamilyRef}>
-              <button onClick={() => toggleDropdown('yourFamily')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 min-[1135px]:w-auto dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1135px]:dark:hover:bg-transparent">your family
+              <button onClick={() => toggleDropdown('yourFamily')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 min-[1210px]:w-auto dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1210px]:dark:hover:bg-transparent">your family
                 <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
@@ -272,7 +273,7 @@ export default function Navbar() {
               )}
             </li>
             <li className="relative" ref={eventRef}>
-              <button onClick={() => toggleDropdown('events')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 min-[1135px]:w-auto dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1135px]:dark:hover:bg-transparent">events
+              <button onClick={() => toggleDropdown('events')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 min-[1210px]:w-auto dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1210px]:dark:hover:bg-transparent">events
                 <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
@@ -304,7 +305,7 @@ export default function Navbar() {
               )}
             </li>
             <li className="relative" ref={connectRef}>
-              <button onClick={() => toggleDropdown('connect')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 min-[1135px]:w-auto dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1135px]:dark:hover:bg-transparent">connect
+              <button onClick={() => toggleDropdown('connect')} className="text-xl flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 min-[1210px]:w-auto dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 min-[1210px]:dark:hover:bg-transparent">connect
                 <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
@@ -362,27 +363,37 @@ export default function Navbar() {
             </li>
             <li>
               <Link href="/stories">
-                <div className={`${isActive('/stories') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1135px]:dark:hover:bg-transparent`}>stories</div>
+                <div className={`${isActive('/stories') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1210px]:dark:hover:bg-transparent`}>stories</div>
               </Link>
             </li>
             <li className='flex items-center'>
-              
+
               {/* NOTE: Give link */}
-              {/* <Link href="https://refresh.churchcenter.com/giving?open-in-church-center-modal=true" target="_blank" rel="noopener noreferrer" className="text-xl block py-2 px-3 me-1 text-primary rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1135px]:dark:hover:bg-transparent">give</Link>
+              {/* <Link href="https://refresh.churchcenter.com/giving?open-in-church-center-modal=true" target="_blank" rel="noopener noreferrer" className="text-xl block py-2 px-3 me-1 text-primary rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1210px]:dark:hover:bg-transparent">give</Link>
               <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
               </svg> */}
-            
+
               <Link href="/give">
-                <div className={`${isActive('/give') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1135px]:hover:bg-transparent min-[1135px]:border-0 min-[1135px]:hover:text-primary min-[1135px]:p-0 dark:text-white min-[1135px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1135px]:dark:hover:bg-transparent`}>give</div>
+                <div className={`${isActive('/give') ? 'primary' : 'gray'} text-gray-900 text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1210px]:dark:hover:bg-transparent`}>give</div>
               </Link>
 
+            </li>
+
+            <li className='text-primary'>
+              <Link className='flex items-center' href="/merch">
+                <div className={` text-xl block py-2 px-3 rounded hover:bg-gray-100 min-[1210px]:hover:bg-transparent min-[1210px]:border-0 min-[1210px]:hover:text-primary min-[1210px]:p-0 dark:text-white min-[1210px]:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white min-[1210px]:dark:hover:bg-transparent`}>merch</div>
+              <ArrowUpRight className="w-4 h-4"/>
+              </Link>
+                {/* <svg className="w-4 h-4  dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
+                </svg> */}
             </li>
           </ul>
         </div>
 
 
-        <div className='items-bottom space-x-1.5 min-[1215px]:flex hidden '>
+        {/* <div className='items-bottom space-x-1.5 min-[1215px]:flex hidden '>
           <a href='https://www.instagram.com/refreshchurchtv/' target="_blank" rel="noopener noreferrer">
             <svg className=" w-8 h-8 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path fill="currentColor" fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd" />
@@ -400,8 +411,7 @@ export default function Navbar() {
               <path fillRule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clipRule="evenodd" />
             </svg>
           </a>
-
-        </div>
+        </div> */}
       </div>
     </nav>
   );
